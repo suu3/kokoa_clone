5d7c42be1ef48b3b4733977ff72e97dc4334dbfb
suu3 <71166763+suu3@users.noreply.github.com>
2021-02-15 03:35:21 +0900

    create clock.js

---
+++ js/clock.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("span");
+
+function getTime(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    clockTitle.innerText =
+        `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+    console.log(innerText);
+}
+
+function init(){
+    getTime();
+    setInterval(getTime,1000);
+}
+
+init();
